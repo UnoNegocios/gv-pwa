e1545b4af829461819add87e01024bc5df20e3c2
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-09 11:14:49 -0600

    test 5

--- components/SocialHead.vue
+++ components/SocialHead.vue
@@ -25,20 +25,60 @@ export default {
     return {
       meta: [
         {
-          //hid: 'og:image',
-          property: 'og:image',
-          content: this.image
-        },/*
-        {
-          hid: 'og:image:width',
-          property: 'og:image:width',
+          hid: 'twitter:title',
+          name: 'twitter:title',
           content: this.title
         },
         {
-          hid: 'og:image:height',
-          property: 'og:image:height',
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: this.description
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: this.image
+        },
+        {
+          hid: 'twitter:image:alt',
+          name: 'twitter:image:alt',
           content: this.title
-        },*/
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: this.title
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.description
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: this.image
+        },
+        {
+          hid: 'og:image:secure_url',
+          property: 'og:image:secure_url',
+          content: this.image
+        },
+        {
+          hid: 'og:image:alt',
+          property: 'og:image:alt',
+          content: this.title
+        },
+        {
+          //hid: 'og:image:width',
+          property: 'og:image:width',
+          content: 600
+        },
+        {
+          //hid: 'og:image:height',
+          property: 'og:image:height',
+          content: 600
+        },
       ]
     }
   }
